6e30f1bf2ecc33ee95d77e3c9e626fda28cde6d1
mapper895 <mapper.895.mp@gmail.com>
2025-09-22 15:23:53 -0600

    Implementacion de todas las rutas

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
 import recipeRoutes from "./routes/recipe.route.js";
+import folderRoutes from "./routes/folder.route.js";
+import categoryRoutes from "./routes/category.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connetDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
@@ -15,6 +17,8 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/recipe", recipeRoutes);
+app.use("/api/v1/folder", folderRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 app.listen(PORT, () => {
   console.log("Servidor listo en el puerto", PORT);
