9b559820f6b120ba5863b27abd92188b7bff878b
mapper895 <mapper.895.mp@gmail.com>
2025-10-14 13:54:37 -0600

    Funcionalidad de la barra buscadora del navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import { CircleUserRound, Menu, Search } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRecipeStore } from "../store/recipeStore";
 
 const Navbar = ({ isMobile, setIsSidebarOpen, user }) => {
+  const { fetchRecipes } = useRecipeStore();
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchRecipes({ search });
+  };
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      if (search.trim().length > 0) {
+        fetchRecipes({ search });
+      } else {
+        fetchRecipes(); // si está vacío, traer todo
+      }
+    }, 400);
+
+    return () => clearTimeout(delay);
+  }, [search]);
+
   return (
     <nav className="bg-primary w-full flex justify-center items-center p-5 text-white">
       <div className="w-full flex justify-between items-center">
@@ -17,10 +38,19 @@ const Navbar = ({ isMobile, setIsSidebarOpen, user }) => {
         )}
 
         {/* Buscador */}
-        <div className="flex items-center justify-start gap-2 p-3 border border-white rounded-xl md:w-[300px] w-1/2">
+        <form
+          onSubmit={handleSearch}
+          className="flex items-center justify-start gap-2 p-3 border border-white rounded-xl md:w-[300px] w-1/2"
+        >
           <Search />
-          <p>Buscar...</p>
-        </div>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full outline-none focus:border-none focus:ring-0"
+          />
+        </form>
       </div>
     </nav>
   );
